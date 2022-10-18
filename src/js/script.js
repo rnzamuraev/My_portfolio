82471c1d6e5a9f190c50a52154320d8e9694422a
Ruslan <rnzamuraev@gmail.com>
2022-10-18 17:39:23 +0300

    added about

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,4 @@
-const menu = require("./parts/menu.js");
-const skill = require("./parts/skills.js");
+window.addEventListener("DOMContentLoaded", () => {
+  const menu = require("./parts/menu.js");
+  const skill = require("./parts/skills.js");
+});
